fa53a8694b3cab1314cd412934a865a0a05dd421
magistrmittil <mitya.2004tarasov.ru@inbox.ru>
2023-06-01 17:21:21 +0300

    code style with string

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,9 +1,11 @@
-#include <iostream>
-
-using namespace std;
+hello_world.cppusing namespace std;
 
 int main() {
 	cout << "Hello, World!" << endl;
+	string name;
+	cin >> name;
+
+	cout << "Hello world from" << " " << name << endl;
 	return 0;
 }
 
